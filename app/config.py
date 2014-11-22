25e9a267d6f4328bdf56ce5c1bc72a9d0f0d2850
Mat Wood <mat@thepacketgeek.com>
2014-11-22 11:26:08 -0800

    Moving to PostgreSQL
    For Heroku deployment eventually

--- app/config.py
+++ app/config.py
@@ -3,8 +3,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SECRET_KEY = '8UsJdJRYm6EhzbdW'
 
-config = Config
+class ProductionConfig(Config):
+    pass
+    
+class DevelopmentConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/cloud-pcap'
